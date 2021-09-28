6ee0996b893ad3bc098b7ab012103b546b4906d2
Kurt Snuggly <unsnugHero@gmail.com>
2021-09-27 21:56:08 -0400

    scroll top top component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Slide, toast, ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 
-import Landing from './components/landing/Landing';
-import LoginCard from './components/login/LoginCard';
-import SignupCard from './components/signup/SignupCard';
-import Header from './components/general/Header';
-import storageService from './services/storage.service';
-
 import { store } from './redux/store';
 import { setAxiosHeaderAuthToken } from './util/helpers';
 import { useEffect } from 'react';
 import { loadUser } from './redux/actions/auth.action';
 import { AUTH_TOKEN_STORAGE_KEY } from './util/constants';
-import Footer from './components/general/Footer';
 import { AppBlackBackgroundContainer, AppContainer } from './components/styles/App.style';
+
+import Landing from './components/landing/Landing';
+import LoginCard from './components/login/LoginCard';
+import SignupCard from './components/signup/SignupCard';
+import Header from './components/general/Header';
+import storageService from './services/storage.service';
+import Footer from './components/general/Footer';
+import ScrollToTop from './components/routing/ScrollToTop';
 
 if (storageService.getItem(AUTH_TOKEN_STORAGE_KEY)) {
   setAxiosHeaderAuthToken(storageService.getItem(AUTH_TOKEN_STORAGE_KEY));
@@ -32,6 +33,7 @@ const App = () => {
     <Provider store={store}>
       <div className='App'>
         <Router>
+          <ScrollToTop />
           <Header />
           <AppBlackBackgroundContainer className='app-black-background-container'>
             <AppContainer className='app-container'>
