8516e69eafc610588d625b12e9c65d79a97f52e4
Kurt Snuggly <unsnugHero@gmail.com>
2021-09-30 01:10:25 -0400

    radio button

---
+++ src/components/general/Radio.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { CustomRadioInput, RadioContainer, RadioInput, RadioLabel } from '../styles/Radio.style';
+
+const Radio = (props) => {
+  return (
+    <RadioContainer className='radio-container'>
+      <RadioLabel className='label'>
+        <RadioInput className='radio' name={props.name} type='radio' onClick={props.onClick} />
+        <CustomRadioInput className='checkmark' />
+        <span style={{ paddingLeft: '16px' }}>{props.text}</span>
+      </RadioLabel>
+    </RadioContainer>
+  );
+};
+
+Radio.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default Radio;
---
+++ src/components/styles/Radio.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { CheckboxContainer, CheckBoxLabel } from './Checkbox.style';
+
+export const RadioContainer = styled(CheckboxContainer)``;
+
+export const RadioLabel = styled(CheckBoxLabel)``;
+
+export const RadioInput = styled.input``;
+
+export const CustomRadioInput = styled.span``;
