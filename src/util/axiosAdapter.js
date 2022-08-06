5af0ec34ba5d0a32503e8f5a7273892950781116
Kurt Snuggly <unsnugHero@gmail.com>
2022-08-06 00:12:34 -0400

    disconnect some stuff

--- src/util/axiosAdapter.js
+++ src/util/axiosAdapter.js
@@ -1,16 +1,8 @@
 import axios from 'axios';
-import AxiosMockAdapter from 'axios-mock-adapter';
-import { mockPollInstance } from './mockServer/mockPoll';
-import { mockRequestInstance } from './mockServer/mockRequest';
-import { mockUserInstance } from './mockServer/mockUser';
 
-const axiosMockInstance = axios.create();
-const axiosLiveInstance = axios.create();
+const isProd = process.env.NODE_ENV === 'production';
+const baseUrl = isProd ? 'http://p5phansite.com' : 'localhost:5000';
 
-const mockAdapter = new AxiosMockAdapter(axiosMockInstance);
-
-mockRequestInstance(mockAdapter);
-mockPollInstance(mockAdapter);
-mockUserInstance(mockAdapter);
+const axiosLiveInstance = axios.create({ baseUrl });
 
 export default axiosLiveInstance;
